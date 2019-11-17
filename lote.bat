971a9cfa1b9cc5805642472d660aeab11a9c6a68
roberto-30 <56975848+roberto-30@users.noreply.github.com>
2019-11-17 15:48:06 -0500

    Add files via upload

---
+++ lote.bat
@@ -0,0 +1,85 @@
+python c_doble01.py "angel" "fresa" 50 "papa" 48
+python c_doble01.py "juan" "arroz" 1.50 "fideo" 1.20
+python c_doble01.py "ana" "pulp" 0.50 "leche" 5.2
+
+python c_doble02.py "angel" 2 45
+python c_doble02.py "carla" 5 50
+python c_doble02.py "mario" 1 15
+
+python c_doble03.py "angel" 5 74.3
+python c_doble03.py "carlos" 45 3.4
+python c_doble03.py "yoel" 1 12.5
+
+python c_doble04.py "rosa" 50 55.5
+python c_doble04.py "carmen" 100 52
+python c_doble04.py "robin" 1 25.5
+
+python c_doble05.py 5 100
+python c_doble05.py 2 20
+python c_doble05.py 2 40
+
+python c_doble06.py 2 25 45
+python c_doble06.py 10 40 54
+python c_doble06.py 1 5 5
+
+python c_doble07.py "angel"  4 2.5
+python c_doble07.py "abigail"  28 2.5
+python c_doble07.py "carla"  10 3.2
+
+python c_doble08.py "angel"  10 3 38 45
+python c_doble08.py "carla"  15 4 45 50
+python c_doble08.py "juan"  15 4 45 200
+
+python c_doble09.py "robert"  15 4
+python c_doble09.py "angel" 2 250
+python c_doble09.py "rosa" 1 45
+
+python c_doble09.py "angel" 2 25 1 45
+python c_doble09.py "robin" 4 20 3 50
+python c_doble09.py "nicol" 1 25 5 25
+
+
+
+python c_multiple01.py "juan" "chocolate" 10 "mango" 2
+python c_multiple01.py "angel" "yogurt" 25 "mesa" 25
+python c_multiple01.py "yhova" "sandalias" 50 "blusa" 40
+
+python c_multiple02.py "carla" 4 50
+python c_multiple02.py "louis" 1 25
+python c_multiple02.py "wily" 2 25
+
+python c_multiple03.py "roberto" 45 250
+python c_multiple03.py "wilson" 4 170
+python c_multiple03.py "jorge" 4 10
+
+python c_multiple04.py "maria" 10 20
+python c_multiple04.py "rosa" 50 110
+python c_multiple04.py "brandy" 1 20
+
+python c_multiple05.py "angel" 4 50
+python c_multiple05.py "will" 10 50
+python c_multiple05.py "nelson" 1 2.3
+
+python c_multiple06.py  10 5 50
+python c_multiple06.py  2 1 2
+python c_multiple06.py  1 2 30
+
+python c_multiple07.py  " angel" 10 5
+python c_multiple07.py  "osmar" 5 2.5
+python c_multiple07.py  "lincol" 20 5
+
+python c_multiple08.py "marcos" 1 2 20 50
+python c_multiple08.py  "anderson" 2 1 7 50
+python c_multiple08.py "luzbany" 1 1 15 50
+
+python c_multiple09.py  10 5 50
+python c_multiple09.py  10 5 50
+python c_multiple09.py  10 5 50
+
+python c_multiple09.py  "jhonatan" 5 100
+python c_multiple09.py  "roberto" 7 150
+python c_multiple09.py  "jhefer" 2 80
+
+python c_multiple10.py  "maria" 5 20 4 80
+python c_multiple10.py  "jhefer" 2 25 2 80
+python c_multiple10.py  "jhefer" 2 80 1 50
---
+++ r_lote.bat
@@ -0,0 +1,80 @@
+python simple-11.py "roberto" 13000 47000
+python simple-11.py "ignacio" 25000 95807
+python simple-11.py "german" 5000 36200
+
+python simple-12.py "alicia" 50 80
+python simple-12.py "ariana" 75 95
+python simple-12.py "angie" 96 20
+
+python simple-13.py "anali" 3 1000
+python simple-13.py "mercedes" 8 2500
+python simple-13.py "nilda" 9 1258
+
+python simple-14.py "manuel y william" 20 10.5
+python simple-14.py "abel y martin" 36 15.2
+python simple-14.py "emanuel y brayan" 28 25.8
+
+python simple-15.py "anahi y angie" 2 4.5
+python simple-15.py "antuantte y erika" 9 6.8
+python simple-15.py "andre y katyana" 5 2.9
+
+python simple-16.py "andrez" 20 53.6
+python simple-16.py "carlos" 50 25.9
+python simple-16.py "elias" 31 5.9
+
+python simple-17.py "fernando" 50.5 2
+python simple-17.py "walter" 25.1 5
+python simple-17.py "jeimi" 35.6 9
+
+python simple-18.py "fiorella" 15 2
+python simple-18.py "yasmin" 17 5
+python simple-18.py "azucena" 21 9
+
+python simple-19.py "yaneli" "billetera" 100 "celular" 250.5
+python simple-19.py "merly" "audifonos" 50 "auriculares" 40.8
+python simple-19.py "yanina" "galletas" 105 "caramelos" 270
+
+python simple-20.py "jean" 20 12.2
+python simple-20.py "anthoni" 2 14.2
+python simple-20.py "homero" 15 50
+
+python doble-11.py "brayton" 10000 50000
+python doble-11.py "kevin" 15241 95841
+python doble-11.py "jerson" 20158 85750
+
+
+python doble-12.py "wilmer" 30 40
+python doble-12.py "cabel" 30 40
+python doble-12.py "smith" 30 40
+
+python doble-13.py "andrez" 500 6
+python doble-13.py "kevin" 625 5
+python doble-13.py "joan" 321 9
+
+python doble-14.py "stefany y sonia" 60 3.5
+python doble-14.py "yohana y mileyde" 95 5.9
+python doble-14.py "varonica y belinda" 85 9.3
+
+python doble-15.py "gloria y shirley" 2 4.5
+python doble-15.py "gardenia y liseth" 7 3.6
+python doble-15.py "grace y viviana" 9 9.7
+
+python doble-16.py "sheyla" 10 5
+python doble-16.py "melissa" 20 4.5
+python doble-16.py "flavia" 85 9.3
+
+python doble-17.py "anhela" 2 60
+python doble-17.py "katy" 9 38
+python doble-17.py "verenice" 5 96
+
+python doble-18.py "roy" 25.5 2
+python doble-18.py "maycol" 95.3 9
+python doble-18.py "mario" 89.1 5
+
+python doble-19.py "jared" "equipo" 100 "escritorio" 150.8
+python doble-19.py "ricardo" "parlantes" 952 "silla" 953.0
+python doble-19.py "eladio" "monitor" 230 "luces" 260.3
+
+python doble-20.py "leydi" 20 4
+python doble-20.py "anderson" 35 9.5
+python doble-20.py "americo" 84 3.6
